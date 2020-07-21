6af5a57a545c201696d7a4ba01f9693bd2a700ae
SwatiSSwain <SWAINSSWATI@GMAIL.COM>
2020-07-21 11:49:35 -0500

    uploading changes

---
+++ static/js/demographic_app.js
@@ -0,0 +1,53 @@
+{/* <script src="https://cdn.plot.ly/plotly-latest.min.js"></script> */}
+
+let demographics_data = demographics
+let income_data = income
+//console.log(demographics_data);
+//console.log(demographics_data["white_pct"]);
+//console.log(income_data);
+
+
+//console.log(labels)
+//console.log(values[0])
+
+demographics_data.forEach(function (incident) {
+  
+  white_pct = parseFloat(incident.white_pct)
+  console.log(white)
+})
+
+//var racepercent = demographics[0];
+//console.log(racepercent);
+//console.log(demographics_data.white_pct);
+
+//demographics_data.forEach(function (demo) {
+  
+// demographics_data.forEach(function (demo) {
+//     let foo = demo.white_pct
+// // INCOME DEMOGRAPHICS BAR CHART
+// var data = [
+//     {
+//       y: ['>35,00', '35-49', '50-74', '75-99', '100+'],
+//       x: [71, 9, 9.2, 3, 7.7],
+//       type: 'bar',
+//       color: 'olive',
+//       orientation: "h"
+//     }
+//   ];
+//   Plotly.newPlot('bar', data);
+// )}; 
+// RACE DEMOGRAPHICS PIE CHRT 
+    // var data = [{
+    //     values: [26.3, 73.7],
+    //     labels: ['White', 'Of Colour'],
+    //     type: 'pie'
+    //   }];
+      
+    //   var layout = {
+    //     height: 400,
+    //     width: 500
+    //   };
+      
+    //   Plotly.newPlot('pie', data, layout);
+
+// Init();
